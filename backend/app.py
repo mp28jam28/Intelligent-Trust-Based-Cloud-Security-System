065a8ab523be12dd2972a8b6c7d5e1df5d47ab62
valeria-campos98 <valeria.campos2055@gmail.com>
2026-03-23 06:30:01 -0700

    setting up env

---
+++ backend/app.py
@@ -0,0 +1,14 @@
+
+# Load environment variables from the .env file
+# We use environment variables instead of hardcoding database credentials
+# We do this for security/standard practice since we are pushing these things to github(which may or may not be public)
+
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+DB_HOST = os.getenv("DB_HOST")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_NAME = os.getenv("DB_NAME")
