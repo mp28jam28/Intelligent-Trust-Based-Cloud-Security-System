71206a69ec1bf441f52cd43005619a499658bd62
mp28jam28 <mp28jam@csu.fullerton.edu>
2026-04-17 18:19:48 -0700

    Added confetti when user sets their sleep goal

--- frontend/js/insights.js
+++ frontend/js/insights.js
@@ -1,6 +1,16 @@
 const slider = document.getElementById("sleepGoalInput")
 const goalValue = document.getElementById("goal-value")
+const button = document.getElementById("setGoalBtn")
+const statusMessage = document.getElementById("statusMessage")
 
 slider.addEventListener("input", () => {
     goalValue.textContent = slider.value + " hrs";
 })
+
+button.addEventListener("click", () => {
+    const goal = slider.value;
+    localStorage.setItem("sleepGoal", goal);
+    statusMessage.textContent = "Goal set to " + goal + " hours";
+    console.log("Goal set to " + goal + "!")
+})
+
